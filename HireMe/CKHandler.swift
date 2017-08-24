d26b226bd4543c110b3266a1888c5396d56a422b
Zac Holland <zacharyholland@gmail.com>
2017-08-24 14:13:25 -0700

    added CKHandler file. Nothing in it yet

---
+++ HireMe/CKHandler.swift
@@ -0,0 +1,13 @@
+//
+//  CKHandler.swift
+//  HireMe
+//
+//  Created by Zac Holland on 8/24/17.
+//  Copyright © 2017 Bandwidth Bandits. All rights reserved.
+//
+
+import Foundation
+
+class CKHandler {
+    
+}
