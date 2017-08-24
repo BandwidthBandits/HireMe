33c1d96224fc9545ccced84c43b4aa73646ea11b
Zac Holland <zacharyholland@gmail.com>
2017-08-24 14:43:38 -0700

    CKHandler can now fetch nearby Job Posts AppDelegate now initializes CKHandler

--- HireMe/CKHandler.swift
+++ HireMe/CKHandler.swift
@@ -7,7 +7,60 @@
 //
 
 import Foundation
+import CloudKit
+
+typealias FetchRecordsCallback = ([CKRecord]?) -> Void
 
 class CKHandler {
+    static var container: CKContainer?
+    static var publicDB: CKDatabase?
+    static var privateDB: CKDatabase?
+    
+    static func initializeCloudkit() {
+        // set up the default container
+        CKHandler.container = CKContainer.default()
+        // get the public database
+        CKHandler.publicDB = CKHandler.container?.publicCloudDatabase
+        // get the private database
+        CKHandler.privateDB = CKHandler.container?.privateCloudDatabase
+    }
+    
+    //NOTE: 1600 meters in a mile
+    static func fetchLocalJobPosts(_ location:CLLocation, radiusInMeters:CLLocationDistance, onComplete: @escaping FetchRecordsCallback) {
+        
+        //make sure public database is set
+        guard let pDB = CKHandler.publicDB else {
+            print("CKHandler ERROR - publicDB is not set! You haven't called initializeCloudKit yet")
+            return
+        }
+        
+        // convert radius in meters to kilometers
+        let radiusInKilometers = radiusInMeters / 1000.0
+        //set up predicate for query
+        let locationPredicate = NSPredicate(format: "distanceToLocation:fromLocation:(%K,%@) < %f", "location", location, radiusInKilometers)
+        //set up query based on predicate
+        let query = CKQuery(recordType: "JobPosts", predicate: locationPredicate)
+        //Execute query
+        CKHandler.publicDB?.perform(query, inZoneWith: nil) { (records, error) in
+            if let error = error {
+                DispatchQueue.main.async {
+                    print("Cloud Query Error - Fetch Establishments: \(error)")
+                }
+                return
+            }
+            
+            onComplete(records)
+            
+        }
+    }
+//    EXAMPLE USE:
+//    let testLocation = CLLocation(latitude: 0, longitude: 0)
+//    
+//    CKHandler.fetchLocalJobPosts(testLocation, radiusInMeters: 1600) { (records) in
+//        records?.forEach({ (record) in
+//            print(record)
+//        })
+//    }
+    
     
 }
