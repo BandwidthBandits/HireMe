a1c42ff1992fafbc21274a4f12b2b2d246b4c7fd
Zac Holland <zacharyholland@gmail.com>
2017-08-28 11:08:14 -0700

    CKHAndler - added price group to createJobPost

--- HireMe/CKHandler.swift
+++ HireMe/CKHandler.swift
@@ -66,13 +66,14 @@ class CKHandler {
     
     
     //Creates a new job post and uploads it to cloudKit
-    static func createJobPost(owner: String, title: String, desc: String, timeEstimate: Int, _ location:CLLocation, onComplete:@escaping UploadRecordCallback, onUploadError:@escaping ErrorCallback ) {
+    static func createJobPost(owner: String, title: String, desc: String, timeEstimate: Int, priceGroup: Int, _ location:CLLocation, onComplete:@escaping UploadRecordCallback, onUploadError:@escaping ErrorCallback ) {
         let newRecord:CKRecord = CKRecord(recordType: "JobPosts")
         newRecord.setValue(owner, forKey: "owner")
         newRecord.setValue(title, forKey: "title")
         newRecord.setValue(desc, forKey: "desc")
         newRecord.setValue(timeEstimate, forKey: "timeEstimate")
         newRecord.setValue(location, forKey: "location")
+        newRecord.setValue(priceGroup, forKey: "priceGroup")
         
         UploadNewRecord(record: newRecord, onComplete: onComplete, onUploadError: onUploadError)
     }
